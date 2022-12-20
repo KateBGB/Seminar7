da8b02199714357ea7acc0230d40f95557d4dec8
KateBGB <113110259+KateBGB@users.noreply.github.com>
2022-12-20 22:15:50 +0300

    48

---
+++ Task48Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] FillArray(int rows, int columns, int start, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = i + j;
+        }
+    }
+    return array;
+}
+
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+//Запрос количества строк
+
+int rows = EnterData("Введение количества строк");
+
+//Запрос количества столбцов
+
+int columns = EnterData("Введение количества столбцов");
+// Создание двухмерного  массива, заполненного случайными числами
+int[,] matrix = FillArray(rows, columns, 0, 10);
+//Выведение массива на экран
+PrintArray(matrix);
