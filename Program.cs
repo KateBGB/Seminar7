d9385c57ee8f7feb554a3cc1c0cd33f6f59df9c5
KateBGB <113110259+KateBGB@users.noreply.github.com>
2022-12-20 22:12:13 +0300

    Task46

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void PrintArray(int [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int[,] FillArray(int rows, int columns, int start, int end)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i,j] = new Random().Next(start, end + 1);
+        }
+    }
+    return array;
+}
+
+
+int EnterData(string text)
+{
+    Console.WriteLine(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+//Запрос количества строк
+
+int rows = EnterData("Введение количества строк");
+
+//Запрос количества столбцов
+
+int columns = EnterData("Введение количества столбцов");
+// Создание двухмерного  массива, заполненного случайными числами
+int[,] matrix = FillArray(rows, columns, 0, 10);
+//Выведение массива на экран
+PrintArray(matrix);
